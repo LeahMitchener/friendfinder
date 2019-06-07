616cbee11e7fb8a71dbea45bf16d89bd228fec06
LeahMitchener <46640686+LeahMitchener@users.noreply.github.com>
2019-06-07 18:50:01 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [
+  {
+    name: "Ahmed",
+    photo: "/ahmed.jpeg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+
+  {
+    name: "Tony",
+    photo: "/tony.jpeg",
+    scores: [4, 2, 3, 3, 2, 3, 5, 4, 3, 2]
+  },
+
+  {
+    name: "Brian",
+    photo: "/brian.jpeg",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+
+  {
+    name: "Clint",
+    photo: "/clint.jpeg",
+    scores: [5, 1, 2, 3, 4, 5, 1, 2, 3, 4]
+  },
+
+  {
+    name: "Lance",
+    photo: "/lance.jpeg",
+    scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+  },
+
+  {
+    name: "Joey",
+    photo: "/joey.jpeg",
+    scores: [4, 5, 1, 2, 3, 4, 5, 1, 2, 3]
+  }
+];
+
+module.exports = friends;
